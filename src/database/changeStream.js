7780d554c85007b20813b93e7f24d9eff3ebe528
Peter <18sp01@gmail.com>
2024-04-21 15:28:56 +0800

    added email feature

--- src/database/changeStream.js
+++ src/database/changeStream.js
@@ -1,138 +1,163 @@
-import { MongoClient } from 'mongodb';
-import util from '../utils/utils.js';
-import fs from 'fs';
-import workerNodeCurd from '../database/crud/workerNodeCurd.js';
-import ticketCrud from '../database/crud/ticketCrud.js';
+import { MongoClient } from "mongodb";
+import util from "../utils/utils.js";
+import fs from "fs";
+import workerNodeCurd from "../database/crud/workerNodeCurd.js";
+import ticketCrud from "../database/crud/ticketCrud.js";
 import Web3 from "web3";
 
-const mongdbUri =`mongodb+srv://howard9:${process.env.PASSWORD}@dmldatabase.vordk9h.mongodb.net/?retryWrites=true&w=majority&appName=DMLdataBase`
-const web3 = new Web3(new Web3.providers.HttpProvider("https://rpc-amoy.polygon.technology/"));
+const mongdbUri = `mongodb+srv://howard9:${process.env.PASSWORD}@dmldatabase.vordk9h.mongodb.net/?retryWrites=true&w=majority&appName=DMLdataBase`;
+const web3 = new Web3(
+  new Web3.providers.HttpProvider("https://rpc-amoy.polygon.technology/")
+);
 const mainNode = process.env.MAINNODE;
-const options = ["./ML/report.txt","./ML/report2.txt","./ML/report3.txt"];
+const options = ["./ML/report.txt", "./ML/report2.txt", "./ML/report3.txt"];
 
 const wallet = web3.eth.accounts.privateKeyToAccount(mainNode);
 const contract = process.env.CONTRACT;
 
 async function startChangeStream() {
-    console.log("startChangeStream");
-    run().catch(console.dir);
+  console.log("startChangeStream");
+  run().catch(console.dir);
 }
 
 async function run() {
-    const client = new MongoClient(mongdbUri);
-    let changeStream;
-      const database = client.db("test");
-      const haikus = database.collection("tickets");
-      // Open a Change Stream on the "haikus" collection
-      changeStream = haikus.watch();
-      // Print change events as they occur
+  const client = new MongoClient(mongdbUri);
+  let changeStream;
+  const database = client.db("test");
+  const haikus = database.collection("tickets");
+  // Open a Change Stream on the "haikus" collection
+  changeStream = haikus.watch();
+  // Print change events as they occur
 
-      for await (const change of changeStream) {
-        if (change.fullDocument !== undefined){
-            //generate a random number from 0 to 2
-            //const random = Math.floor(Math.random() * 3);
-            const filepath = './ML/report.txt'
-          console.log("Received data in cloud for user: ", change.fullDocument.userId);
-          console.log("Ready to proceed Machine Learning")
-          //util.runBashCommand("python3 ./ML/emnist_fedavg_main.py");
-          while (true){
-              if (fs.existsSync(filepath)){
-                  break;
-              }
-          }
-          let data = fs.readFileSync(filepath, 'utf8');
-          data = data.replace(/'/g, '"');
-          console.log(data)
-          data = JSON.parse(data);
-          console.log("Machine Learning is done return status: ", data.status);
-          if (data.status === "success"){
-            let metadatas = JSON.parse(fs.readFileSync("./ML/mo.txt", 'utf8'));
-            await ticketCrud.updateTicket({
-                userId: change.fullDocument.userId,
-                transactionHash: change.fullDocument.transactionHash,
-                uploadStatus: "completed",
-            });
-            let walletAddresses = [];
-            for (let i in data.data){
-                try{
-                    const wallet = await workerNodeCurd.getUserWallet(i);
-                    walletAddresses.push(wallet.walletAddress);
-                }catch(err){
-                    console.log(err)
-                }
-        
-            }
-            console.log(walletAddresses)
-            const txHashes = await util.giveReward(web3, wallet,metadatas, change.fullDocument.transactionHash, contract, walletAddresses);
-            console.log("Rewarded node providers accordingly: ", txHashes);
-            let params = "Your ticket has been completed successfully";
-            let title = "Training Complete!";
-            let receiver = change.fullDocument.userId
-            await util.sendEmail(params,title,receiver)
-          }else{
-            console.log("Malicious activity detected in the node providers")
-            let count = 0;
-            for (const node in data.data){
-            if (data.data[node]=="fail"){
-                try{
-                    const result =await workerNodeCurd.updateWallet({
-                        userId: node,
-                        status: "banned"
-                    });
-                    console.log("malicious node provider ", node, " has been banned")
-                    count += 1;
-                }catch(err){
-                    console.log("error")
-                }
-            }
-            }
-            let rewardAmount = 0.001*count;
-            rewardAmount = rewardAmount/(Object.keys(data.data).length-count);
-            console.log("user will get back token")
-            try{
-                console.log(change.fullDocument.userId)
-                const userInfo = await ticketCrud.getTicket({userId: change.fullDocument.userId, uploadStatus: "uploaded"});
-                console.log(userInfo)
-                await util.transfer(web3, wallet, userInfo.walletAddress, contract, userInfo.amount);
-                console.log("transfer done")
-                await ticketCrud.updateTicket({
-                    userId: change.fullDocument.userId,
-                    uploadStatus: "rollback",
-                    transactionHash: change.fullDocument.transactionHash
-                });
-                console.log("deposited coin will be distributed to other node provider")
-                try{
-                    for (const node in data.data){
-                        if (data.data[node]=="success"){
-                            console.log("paying node provider : ",node)
-                            console.log("reward amount: ",rewardAmount)
-                            let receiver = await workerNodeCurd.getUserWallet(node);
-                            await util.payNativeToken(web3,wallet.address,receiver.walletAddress,wallet.privateKey,rewardAmount);
-                        }
-                    }
-                    let params = "Your ticket has been rollbacked due to malicious activity in the node provider";
-                    let title = "Ticket Rollbacked";
-                    let receiver = change.fullDocument.userId
-                    await util.sendEmail(params,title,receiver)
-                }catch(err){
-                    console.log(err)
-                }
-            }
-            catch(err){
-                console.log("error: ticket does not exist")
-            }
-          }
-        //check for malicious node provider (check by meta data)
-
-
+  for await (const change of changeStream) {
+    if (change.fullDocument !== undefined) {
+      //generate a random number from 0 to 2
+      //const random = Math.floor(Math.random() * 3);
+      const filepath = "./ML/report.txt";
+      console.log(
+        "Received data in cloud for user: ",
+        change.fullDocument.userId
+      );
+      console.log("Ready to proceed Machine Learning");
+      //util.runBashCommand("python3 ./ML/emnist_fedavg_main.py");
+      while (true) {
+        if (fs.existsSync(filepath)) {
+          break;
         }
       }
-      // Close the change stream when done
-
+      let data = fs.readFileSync(filepath, "utf8");
+      data = data.replace(/'/g, '"');
+      console.log(data);
+      data = JSON.parse(data);
+      console.log("Machine Learning is done return status: ", data.status);
+      if (data.status === "success") {
+        let metadatas = JSON.parse(fs.readFileSync("./ML/mo.txt", "utf8"));
+        await ticketCrud.updateTicket({
+          userId: change.fullDocument.userId,
+          transactionHash: change.fullDocument.transactionHash,
+          uploadStatus: "completed",
+        });
+        let walletAddresses = [];
+        for (let i in data.data) {
+          try {
+            const wallet = await workerNodeCurd.getUserWallet(i);
+            walletAddresses.push(wallet.walletAddress);
+          } catch (err) {
+            console.log(err);
+          }
+        }
+        console.log(walletAddresses);
+        const txHashes = await util.giveReward(
+          web3,
+          wallet,
+          metadatas,
+          change.fullDocument.transactionHash,
+          contract,
+          walletAddresses
+        );
+        console.log("Rewarded node providers accordingly: ", txHashes);
+        let params =
+          "Dear Customer,\n\nThank you for using the Decentralized Machine Learning Platform.\nHere are the links for the updated model and weights for you to download.\nhttps://storage.googleapis.com/file-bucket93/model.json\nhttps://storage.googleapis.com/file-bucket93/model.h5\n\nThank you,\nDML Team";
+        let title = "Your Model is Availabe for Download";
+        let receiver = change.fullDocument.userId;
+        await util.sendEmail(params, title, receiver);
+      } else {
+        console.log("Malicious activity detected in the node providers");
+        let count = 0;
+        for (const node in data.data) {
+          if (data.data[node] == "fail") {
+            try {
+              const result = await workerNodeCurd.updateWallet({
+                userId: node,
+                status: "banned",
+              });
+              console.log("malicious node provider ", node, " has been banned");
+              count += 1;
+            } catch (err) {
+              console.log("error");
+            }
+          }
+        }
+        let rewardAmount = 0.001 * count;
+        rewardAmount = rewardAmount / (Object.keys(data.data).length - count);
+        console.log("user will get back token");
+        try {
+          console.log(change.fullDocument.userId);
+          const userInfo = await ticketCrud.getTicket({
+            userId: change.fullDocument.userId,
+            uploadStatus: "uploaded",
+          });
+          console.log(userInfo);
+          await util.transfer(
+            web3,
+            wallet,
+            userInfo.walletAddress,
+            contract,
+            userInfo.amount
+          );
+          console.log("transfer done");
+          await ticketCrud.updateTicket({
+            userId: change.fullDocument.userId,
+            uploadStatus: "rollback",
+            transactionHash: change.fullDocument.transactionHash,
+          });
+          console.log(
+            "deposited coin will be distributed to other node provider"
+          );
+          try {
+            for (const node in data.data) {
+              if (data.data[node] == "success") {
+                console.log("paying node provider : ", node);
+                console.log("reward amount: ", rewardAmount);
+                let receiver = await workerNodeCurd.getUserWallet(node);
+                await util.payNativeToken(
+                  web3,
+                  wallet.address,
+                  receiver.walletAddress,
+                  wallet.privateKey,
+                  rewardAmount
+                );
+              }
+            }
+            let params =
+              "Dear Customer,\n\nUnfortunately, there has been data mishandling by our node providers while performing machine learning on your data. These providers have been banned and will not operate on our platform anymore.\nThe full amount of your payment has been refunded to your wallet address. Please try uploading your data at a later time. We apologize for the inconvenience.\n\nThank you,\nDML Team";
+            let title = "Machine Learning Failed";
+            let receiver = change.fullDocument.userId;
+            await util.sendEmail(params, title, receiver);
+          } catch (err) {
+            console.log(err);
+          }
+        } catch (err) {
+          console.log("error: ticket does not exist");
+        }
+      }
+      //check for malicious node provider (check by meta data)
+    }
   }
- 
+  // Close the change stream when done
+}
 
 const stream = {
-    startChangeStream
-}
+  startChangeStream,
+};
 export default stream;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,110 +1,132 @@
-import {exec} from 'child_process';
+import { exec } from "child_process";
 import TokenABI from "../../contractABI/tokenABI.js";
 import BigNumber from "bignumber.js";
 import Web3 from "web3";
-import { SMTPClient } from 'emailjs';
-import c from 'args';
+import { SMTPClient } from "emailjs";
+import 'dotenv/config';
+import c from "args";
 
 async function runBashCommand(command) {
-    return new Promise((resolve, reject) => {
-        exec(command, (err, stdout, stderr) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            }
-            resolve(stdout ? stdout : stderr);
-        });
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      resolve(stdout ? stdout : stderr);
     });
+  });
 }
 
 async function transfer(web3, sender, recipient, contract, amount) {
-    try {
-      // Get the number of tokens to transfer (convert from wei to token decimals)
-      const tokenCrt = new web3.eth.Contract(TokenABI.abi, contract);
-      const tokenDecimals = await tokenCrt.methods.decimals().call();
-      amount = amount * Math.pow(10, tokenDecimals);
-      amount = BigNumber(amount);
-  
-      // Transfer tokens from the owner's account to the recipient
-      const gasPrice = await web3.eth.getGasPrice();
-      const gasLimit = 150000;
-      const nonce = await web3.eth.getTransactionCount(sender.address, 'latest');
-  
-      const txObject = {
-        gasLimit: Web3.utils.toHex(gasLimit),
-        gasPrice: Web3.utils.toHex(gasPrice),
-        from: sender.address,
-        to: contract,
-        nonce: nonce,
-        data: tokenCrt.methods.transfer(recipient, amount).encodeABI(),
-      };
-  
-      // Sign and send the transaction
-      const signedTx = await web3.eth.accounts.signTransaction(txObject, sender.privateKey);
-      const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-      return receipt.logs[0].transactionHash;
-      console.log(receipt.logs[0]);
-    } catch (error) {
-      console.error('Error transferring tokens:', error);
-    }
+  try {
+    // Get the number of tokens to transfer (convert from wei to token decimals)
+    const tokenCrt = new web3.eth.Contract(TokenABI.abi, contract);
+    const tokenDecimals = await tokenCrt.methods.decimals().call();
+    amount = amount * Math.pow(10, tokenDecimals);
+    amount = BigNumber(amount);
+
+    // Transfer tokens from the owner's account to the recipient
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasLimit = 150000;
+    const nonce = await web3.eth.getTransactionCount(sender.address, "latest");
+
+    const txObject = {
+      gasLimit: Web3.utils.toHex(gasLimit),
+      gasPrice: Web3.utils.toHex(gasPrice),
+      from: sender.address,
+      to: contract,
+      nonce: nonce,
+      data: tokenCrt.methods.transfer(recipient, amount).encodeABI(),
+    };
+
+    // Sign and send the transaction
+    const signedTx = await web3.eth.accounts.signTransaction(
+      txObject,
+      sender.privateKey
+    );
+    const receipt = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction
+    );
+    return receipt.logs[0].transactionHash;
+    console.log(receipt.logs[0]);
+  } catch (error) {
+    console.error("Error transferring tokens:", error);
+  }
 }
 
-async function getFileSize(filePath) { 
-    const stats = fs.statSync(filePath);
-    if(stats.isFile()) {
-      const size = stats.size;
-      console.log(`${fileName} size: ${Math.round(size/1024)} KB`);
-      return Math.round(size/1024)
-    }
-    return "File not found"
+async function getFileSize(filePath) {
+  const stats = fs.statSync(filePath);
+  if (stats.isFile()) {
+    const size = stats.size;
+    console.log(`${fileName} size: ${Math.round(size / 1024)} KB`);
+    return Math.round(size / 1024);
+  }
+  return "File not found";
 }
 
-async function payByFile(web3, sender, recipient, contract, filesize){
-    let amount = filesize * 0.0001;
-    console.log(`Amount to be paid: ${amount} tokens`);
-    const result = await transfer(web3, sender, recipient, contract, amount);
-    console.log(result);
+async function payByFile(web3, sender, recipient, contract, filesize) {
+  let amount = filesize * 0.0001;
+  console.log(`Amount to be paid: ${amount} tokens`);
+  const result = await transfer(web3, sender, recipient, contract, amount);
+  console.log(result);
 }
 
-async function getTotalReward(web3, transactionHash){
+async function getTotalReward(web3, transactionHash) {
   const transaction = await web3.eth.getTransactionReceipt(transactionHash);
-  const tokenValue = new BigNumber(transaction.logs[0].data).dividedBy('1e18').toNumber();
-  console.log(`Transaction value: ${(tokenValue*0.98).toFixed(2)} tokens`);
-  return (tokenValue*0.98).toFixed(2);
+  const tokenValue = new BigNumber(transaction.logs[0].data)
+    .dividedBy("1e18")
+    .toNumber();
+  console.log(`Transaction value: ${(tokenValue * 0.98).toFixed(2)} tokens`);
+  return (tokenValue * 0.98).toFixed(2);
 }
 
-async function giveReward(web3,wallet,  metadatas, transactionHash, tokenAddress, walletAddresses){
+async function giveReward(
+  web3,
+  wallet,
+  metadatas,
+  transactionHash,
+  tokenAddress,
+  walletAddresses
+) {
   let rewards = [];
   let result = [];
   let available_token = await getTotalReward(web3, transactionHash);
   for (let i = 0; i < metadatas.length; i++) {
-      const workernode = metadatas[i];
-      const reward = calculateReward(workernode);
-      rewards.push(reward);
+    const workernode = metadatas[i];
+    const reward = calculateReward(workernode);
+    rewards.push(reward);
   }
   //calculate the total reward
   let total_reward = 0;
   for (let i = 0; i < rewards.length; i++) {
-      total_reward += rewards[i];
+    total_reward += rewards[i];
   }
   //calculate the reward for each workernode
   for (let i = 0; i < rewards.length; i++) {
-      rewards[i] = rewards[i]/total_reward * available_token;
-      console.log(roundDown(rewards[i],2) + " tokens");
-      try{
-        const txHash =await transfer(web3, wallet, walletAddresses[i], tokenAddress, roundDown(rewards[i],2));
-        result.push(txHash);
-      }catch(err){
-        console.log(err);
-      }
-
+    rewards[i] = (rewards[i] / total_reward) * available_token;
+    console.log(roundDown(rewards[i], 2) + " tokens");
+    try {
+      const txHash = await transfer(
+        web3,
+        wallet,
+        walletAddresses[i],
+        tokenAddress,
+        roundDown(rewards[i], 2)
+      );
+      result.push(txHash);
+    } catch (err) {
+      console.log(err);
+    }
   }
   return result;
-
 }
 
-function calculateReward(metadata){
-  const accuracy = metadata.sparse_categorical_accuracy[0]/metadata.sparse_categorical_accuracy[1] * 100;
+function calculateReward(metadata) {
+  const accuracy =
+    (metadata.sparse_categorical_accuracy[0] /
+      metadata.sparse_categorical_accuracy[1]) *
+    100;
   const reward = accuracy;
   return reward;
 }
@@ -112,86 +134,121 @@ function roundDown(number, decimalPlaces) {
   const factor = Math.pow(10, decimalPlaces);
   return Math.floor(number * factor) / factor;
 }
-async function payNativeToken(web3, senderAddress, receiverAddress, senderPrivateKey, amount){
+async function payNativeToken(
+  web3,
+  senderAddress,
+  receiverAddress,
+  senderPrivateKey,
+  amount
+) {
   amount = roundDown(amount, 6);
-	web3.eth.accounts.wallet.add(senderPrivateKey);
-	const gasPrice = await web3.eth.getGasPrice();
-	const gasLimit = 100000;
-	const nonce = await web3.eth.getTransactionCount(senderAddress, 'latest');
+  web3.eth.accounts.wallet.add(senderPrivateKey);
+  const gasPrice = await web3.eth.getGasPrice();
+  const gasLimit = 100000;
+  const nonce = await web3.eth.getTransactionCount(senderAddress, "latest");
 
-	return new Promise((resolve) =>{
-		const txn = web3.eth.sendTransaction({
-			gasLimit: web3.utils.toHex(gasLimit),
-			gasPrice: web3.utils.toHex(gasPrice),
-			from: senderAddress,
-			to: receiverAddress,
-			nonce: nonce,
-			value: web3.utils.toWei(amount.toString(), "ether"),
-		})
-		txn.once("receipt", (receipt) => {
-			var gasFee = receipt.gasUsed * receipt.effectiveGasPrice;
-			gasFee = web3.utils.fromWei(gasFee.toString())
-			var status;
-			if(receipt.status){
-			  status = "Success"
-			}
-			else{
-			  status = "Failed"
-			}
-			const response = {
-			  status: status,
-			  message:{
-				  transactionId: receipt.transactionHash,
-				  gasFee: Number(gasFee),
-				  senderAddress: senderAddress,
-				  receiverAddress: receiverAddress,
-				  amount: amount
-			  }
-			}
-			resolve(response);
-		});
-		txn.on("error", (error) =>{
-			const response = {
-				status: "Failed",
-				message: error.message
-			}
-			resolve(response);
-		});
-		web3.eth.accounts.wallet.remove(senderAddress);
-	});
+  return new Promise((resolve) => {
+    const txn = web3.eth.sendTransaction({
+      gasLimit: web3.utils.toHex(gasLimit),
+      gasPrice: web3.utils.toHex(gasPrice),
+      from: senderAddress,
+      to: receiverAddress,
+      nonce: nonce,
+      value: web3.utils.toWei(amount.toString(), "ether"),
+    });
+    txn.once("receipt", (receipt) => {
+      var gasFee = receipt.gasUsed * receipt.effectiveGasPrice;
+      gasFee = web3.utils.fromWei(gasFee.toString());
+      var status;
+      if (receipt.status) {
+        status = "Success";
+      } else {
+        status = "Failed";
+      }
+      const response = {
+        status: status,
+        message: {
+          transactionId: receipt.transactionHash,
+          gasFee: Number(gasFee),
+          senderAddress: senderAddress,
+          receiverAddress: receiverAddress,
+          amount: amount,
+        },
+      };
+      resolve(response);
+    });
+    txn.on("error", (error) => {
+      const response = {
+        status: "Failed",
+        message: error.message,
+      };
+      resolve(response);
+    });
+    web3.eth.accounts.wallet.remove(senderAddress);
+  });
 }
-async function sendEmail(params,title, receiver){
-  try{
-      const client = new SMTPClient({
-          user: process.env.EMAIL,
-          password: process.env.EMAIL_PASSWORD,
-          host: process.env.EMAIL_SMTP,
-          ssl: true,
-      });
-      client.send(
-          {
-              text: params,
-              from: process.env.EMAIL,
-              to: receiver,
-              subject: title,
-          }
-      )
-      return client
-  } catch (e){
-      console.log("Error sending email, retrying...................................................................................")
+
+import nodemailer from "nodemailer";
+async function sendEmail(params, title, receiver) {
+  try {
+    const client = new SMTPClient({
+      user: process.env.EMAIL,
+      password: process.env.EMAIL_PASSWORD,
+      host: process.env.EMAIL_SMTP,
+      ssl: true,
+    });
+    client.send({
+      text: params,
+      from: process.env.EMAIL,
+      to: receiver,
+      subject: title,
+    });
+    return client;
+  } catch (e) {
+    console.log("Error sending email, retrying..." + e);
   }
+  // Create a transporter
+  // let transporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   // host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     type: "OAUTH2",
+  //     user: process.env.EMAIL, //set these in your .env file
+  //     clientId: process.env.OAUTH_CLIENT_ID,
+  //     clientSecret: process.env.OAUTH_CLIENT_SECRET,
+  //     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  //     accessToken: process.env.OAUTH_ACCESS_TOKEN,
+  //     expires: 3599,
+  //   },
+  // });
+  // // Define the email options
+  // let mailOptions = {
+  //   from: process.env.EMAIL,
+  //   to: receiver,
+  //   subject: title,
+  //   text: params,
+  // };
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log("Email sent: " + info.response);
+  //   }
+  // });
 }
 
 const util = {
-    runBashCommand,
-    transfer,
-    getFileSize,
-    payByFile,
-    getTotalReward,
-    giveReward,
-    calculateReward,
-    payNativeToken,
-    sendEmail,
+  runBashCommand,
+  transfer,
+  getFileSize,
+  payByFile,
+  getTotalReward,
+  giveReward,
+  calculateReward,
+  payNativeToken,
+  sendEmail,
 };
 
 export default util;
